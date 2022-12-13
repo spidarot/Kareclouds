1bab608c0ff1041e345500f9289f7ea3070e57f2
spidarot <117645823+spidarot@users.noreply.github.com>
2022-12-13 15:03:31 +0530

    Add files via upload

--- src/test/java/testScripts/TestScript1.java
+++ src/test/java/testScripts/TestScript1.java
@@ -4,11 +4,18 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Properties;
 
+import javax.print.attribute.standard.Chromaticity;
+
 import org.apache.commons.logging.Log;
 import org.openqa.selenium.WebDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import objectFiles.ChargeCategoryPage;
+import objectFiles.ChargeTypePage;
+import objectFiles.DisabledPatientPage;
+import objectFiles.DoctorOPDChargePage;
+import objectFiles.HospitalChargesPage;
 import objectFiles.LoginPage;
 import objectFiles.PatientPage;
 import objectFiles.SetupPage;
@@ -30,7 +37,7 @@ public void loginAndPatientPageTest() throws IOException, InterruptedException {
 	
 	SetupPage setup =login.loginApplicatio(prop.getProperty("Username"), prop.getProperty("Password"));
 	setup.setupClick();
-	PatientPage patient=setup.LoadpatientPage();
+	PatientPage patient=setup.loadPatientPage();
 	String CurrentUrl=patient.getCurrentUrl();
 	Assert.assertEquals(CurrentUrl, prop.getProperty("patientPagetUrl"),"Url Mismatch" );
 	
@@ -90,7 +97,7 @@ public void loginAndPatientPageTest() throws IOException, InterruptedException {
 public void sorting() throws InterruptedException {
 	SetupPage setup =login.loginApplicatio(prop.getProperty("Username"), prop.getProperty("Password"));
 	setup.setupClick();
-	PatientPage patient=setup.LoadpatientPage();
+	PatientPage patient=setup.loadPatientPage();
 	String sortBy=prop.getProperty("sortBy");
 	patient.SortingButtonElementClick(sortBy);
 	patient.getSortingOrder(sortBy);
@@ -108,18 +115,136 @@ public void sorting() throws InterruptedException {
 public void editPatientData() throws IOException {
 	SetupPage setup =login.loginApplicatio(prop.getProperty("Username"), prop.getProperty("Password"));
 	setup.setupClick();
-	PatientPage patient=setup.LoadpatientPage();
+	PatientPage patient=setup.loadPatientPage();
 	patient.showPatientDataWindow();
 	patient.editPatientDataButtonClick();
 	
 }
 
 @Test
-public void deletPatientData() throws IOException {
+public void deletPatientData() throws IOException, InterruptedException {
 	SetupPage setup =login.loginApplicatio(prop.getProperty("Username"), prop.getProperty("Password"));
 	setup.setupClick();
-	PatientPage patient=setup.LoadpatientPage();
-	patient.disabledPatientListButtonClick();
+	PatientPage patient=setup.loadPatientPage();
+	DisabledPatientPage disabledPatient=patient.disabledPatientListButtonClick();
+	disabledPatient.screenshot("deletPatientData1");
+	disabledPatient.patientDataElementClick();
+	disabledPatient.deletePatientData();
+	disabledPatient.alertHandlingPositive();
+	Thread.sleep(200);
+	disabledPatient.screenshot("deletPatientData2");
+	
+	//enable Patient data
+	disabledPatient.patientDataElementClick();
+	disabledPatient.enablePatientDataButtonClick();
+	disabledPatient.alertHandlingPositive();
+	Assert.assertEquals(disabledPatient.toastMessageGetText(), "Record Active");
+	System.out.println(disabledPatient.toastMessageGetText());
+	disabledPatient.closePatientDataButtonclick();
+	disabledPatient.screenshot("enablePatientData2");
+	
+	//patient List Button working
+	disabledPatient.patientListButtonClick();
+}
+
+//Hospital Charges add charge
+@Test
+public void hospitalChargesaddcharge() throws IOException, InterruptedException {
+	SetupPage setup =login.loginApplicatio(prop.getProperty("Username"), prop.getProperty("Password"));
+	setup.setupClick();
+	HospitalChargesPage hospitalCharges=setup.loadHospitalChargesPage();
+	hospitalCharges.addChargeButtonClick();
+	hospitalCharges.selectChargeTypeOption("Investigations");
+	//hospitalCharges.selectChargeCategoryOption("Select");
+	hospitalCharges.enterCode("1011");
+	hospitalCharges.enterStandardChargeAmount("200");
+	hospitalCharges.applyAllButtonClick();
+	hospitalCharges.enterDescription("pay me");
+	hospitalCharges.saveButtonClick("hospitalChargesaddcharge");
+	Assert.assertEquals(hospitalCharges.toastMessageGetText(), "The Charge Category field is required.");
+	System.out.println(hospitalCharges.toastMessageGetText());
+	hospitalCharges.closeAddChargeButtonClick();
+	hospitalCharges.showChargeDetailswindowButtonclick(1);
+	Thread.sleep(2000);
+	hospitalCharges.screenshot("Charge Details window");
+	hospitalCharges.closeChargeDetailsWindowButtonclick();
+	Thread.sleep(2000);
+	hospitalCharges.editChargeDetailsWindowButtonclick(8);
+	hospitalCharges.screenshot("Edit Charge Details Window");
+	hospitalCharges.saveEditChargeDetailsWindowButtonclick();
+	hospitalCharges.closeEditChargeDetailsWindowButtonclick();
+	Thread.sleep(2000);
+	hospitalCharges.deleteChargeDetailsButtonclick(1);
+	hospitalCharges.alertHandlingNegative();
 	
 }
+//Charge Category
+@Test
+public void ChargeCategory() throws IOException, InterruptedException {
+	SetupPage setup =login.loginApplicatio(prop.getProperty("Username"), prop.getProperty("Password"));
+	setup.setupClick();
+	HospitalChargesPage hospitalCharges=setup.loadHospitalChargesPage();
+	ChargeCategoryPage chargeCategory=hospitalCharges.loadChargeCategoryPage();
+	chargeCategory.addChargeCategoryButtonClick();
+	chargeCategory.enterNameInAddChargeCategory("chargeName");
+	chargeCategory.enterDescriptionInAddChargeCategory("charge discription");
+	chargeCategory.selectChargeTypeInAddChargeCategory("Supplier");
+	chargeCategory.saveAddChargeCategoryButtonClick();
+	Thread.sleep(2000);
+	chargeCategory.editChargeCategoryButtonClick(33);
+	chargeCategory.screenshot("Edit Charge Category");
+	chargeCategory.saveEditChargeCategoryButtonClick();
+	System.out.println("saved Edit Charge Category");
+	Thread.sleep(2000);
+	chargeCategory.deleteChargeCategoryButtonClick(33);
+	chargeCategory.alertHandlingPositive();
+	System.out.println("deleted Charge Category successfully");
+
+}
+//Doctor OPD Charges
+@Test
+public void doctorOPDChargeTest() throws InterruptedException, IOException {
+	SetupPage setup =login.loginApplicatio(prop.getProperty("Username"), prop.getProperty("Password"));
+	setup.setupClick();
+	HospitalChargesPage hospitalCharges=setup.loadHospitalChargesPage();
+	DoctorOPDChargePage doctorOPDCharge=hospitalCharges.loadDoctorOPDChargePage();
+	doctorOPDCharge.addChargeButtonClick();
+	doctorOPDCharge.showDoctorOption();
+	doctorOPDCharge.selectDoctorOption("Aditya");
+	doctorOPDCharge.enterStandardCharges("122");
+	doctorOPDCharge.applyToAllButtonClick();
+	doctorOPDCharge.screenshot("Add Charge");
+	doctorOPDCharge.saveButtonClick();
+	Assert.assertEquals(hospitalCharges.toastMessageGetText(), "Record already exists");
+	System.out.println(doctorOPDCharge.toastMessageGetText());
+	Thread.sleep(3000);
+	doctorOPDCharge.closeAddChargeButtonClick();
+	Thread.sleep(3000);
+	//view charge details
+	doctorOPDCharge.viewChargeDetailsButtonClick(3);
+	doctorOPDCharge.screenshot("View Charge Details");
+	doctorOPDCharge.closeViewChargeDetailsButtonClick();
+	Thread.sleep(3000);
+	//edit charge details
+	doctorOPDCharge.editChargeDetailsButtonClick(3);
+	doctorOPDCharge.screenshot("Edit Charge Details");
+	doctorOPDCharge.saveEditButtonClick();
+	//doctorOPDCharge.deleteChargeDetailsButtonClick(3);
+	
+}
+@Test
+public void ChargeTypePageTest() throws InterruptedException {
+	SetupPage setup =login.loginApplicatio(prop.getProperty("Username"), prop.getProperty("Password"));
+	setup.setupClick();
+	HospitalChargesPage hospitalCharges=setup.loadHospitalChargesPage();
+	ChargeTypePage chargeType =hospitalCharges.loadChargeTypePage();
+	chargeType.addChargeTypeButtonClick();
+	chargeType.enterChargeType("chargesssss");
+	chargeType.saveAddChargeTypeButtonClick();
+	Thread.sleep(3000);
+	chargeType.deleteChargeTypeButtonClick(33);
+	
+	
+}
+
 }
