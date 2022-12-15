5c034b3ca54521b3605caae852c1acd6a0702837
spidarot <117645823+spidarot@users.noreply.github.com>
2022-12-15 15:28:39 +0530

    Add files via upload

--- src/main/java/abstractParentFiles/ObjectParentClass.java
+++ src/main/java/abstractParentFiles/ObjectParentClass.java
@@ -3,6 +3,8 @@ package abstractParentFiles;
 import java.io.File;
 import java.io.IOException;
 import java.time.Duration;
+import java.util.NoSuchElementException;
+import java.util.function.Function;
 
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
@@ -13,7 +15,9 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
 import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.Wait;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class ObjectParentClass {
@@ -32,7 +36,18 @@ public class ObjectParentClass {
 		WebDriverWait w=new WebDriverWait(driver,Duration.ofSeconds(10));
 		w.until(ExpectedConditions.visibilityOf(webElment));
 	}
+	public void waitsFluent(WebElement webElment) {
 	
+	Wait<WebDriver> w=new FluentWait<WebDriver>(driver).withTimeout(Duration.ofSeconds(10)).pollingEvery(Duration.ofMillis(10)).ignoring(NoSuchElementException.class);
+	WebElement obj=w.until(new Function<WebDriver,WebElement>(){
+	public WebElement apply(WebDriver driver) {
+	if(webElment.isDisplayed()) {
+	return webElment;	
+	}
+	else return null;
+	}
+	});
+	}
 	public  void screenshot(String FileName) throws IOException 
 	{
 		String FName=FileName;
@@ -45,8 +60,17 @@ public class ObjectParentClass {
 		Actions a = new Actions(driver);
 		a.moveToElement(findbyElement).build().perform();
 	}
+	public void actionScroll(WebElement findbyElement) 
+	{
+		Actions a = new Actions(driver);
+		a.scrollToElement(findbyElement).build().perform();
+	}
 	public void selectOption(WebElement findbyElement,String selectByValue) {
 		Select s = new Select(findbyElement);
 		s.selectByValue(selectByValue);
+	}
+	public void selectOptionByText(WebElement findbyElement,String selectByText) {
+		Select s = new Select(findbyElement);
+		s.selectByVisibleText(selectByText);
 	}
 }
